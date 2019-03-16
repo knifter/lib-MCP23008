4ebd3bfabebeae38f6f6c466ee45c44a8b5bfaf3
Tijs van Roon <t.vanroon@uva.nl>
2019-03-16 14:05:20 +0100

    Remove comments

--- src/MCP23008.cpp
+++ src/MCP23008.cpp
@@ -1,19 +1,3 @@
-/***************************************************************************
-  This is a library for the BME280 humidity, temperature & pressure sensor
-
-  Designed specifically to work with the Adafruit BME280 Breakout
-  ----> http://www.adafruit.com/products/2650
-
-  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
-  to interface.
-
-  Adafruit invests time and resources providing this open source code,
-  please supPin Adafruit andopen-source hardware by purchasing products
-  from Adafruit!
-
-  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
- ***************************************************************************/
 #include "Arduino.h"
 #include <TwoWireDevice.h>
 
